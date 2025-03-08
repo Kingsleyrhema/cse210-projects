8b581e49be8d57a91392bf7fda1832290fbdd73f
Kingsleyrhema <kingsleyrhema135@gmail.com>
2025-03-08 23:11:18 +0100

    Completed Week01 excercises

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        string playAgain = "yes";
+        
+        while (playAgain.ToLower() == "yes")
+        {
+            int magicNumber = random.Next(1, 101);
+            int guess = 0;
+            int attempts = 0;
+            
+            Console.WriteLine("Welcome to Guess My Number!");
+            
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+                
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {attempts} attempts!");
+                }
+            }
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+        
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            if (int.TryParse(Console.ReadLine(), out int num))
+            {
+                if (num == 0)
+                    break;
+
+                numbers.Add(num);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        if (numbers.Count > 0)
+        {
+            int sum = numbers.Sum();
+            double average = numbers.Average();
+            int max = numbers.Max();
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+
+            // Stretch Challenge
+            int? smallestPositive = numbers.Where(n => n > 0).OrderBy(n => n).FirstOrDefault();
+            if (smallestPositive.HasValue)
+            {
+                Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+            }
+
+            numbers.Sort();
+            Console.WriteLine("The sorted list is:");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
